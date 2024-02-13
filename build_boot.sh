2d7d5a254ed5e83f6bc8a8f45ca3f364cc4494a8
theorlangur <theorlangur@gmail.com>
2024-02-13 20:46:29 +0100

    enabled floating point support

--- build_boot.sh
+++ build_boot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-clang --target=arm-none-eabihf -mcpu=arm1176jzf-s -fpic -ffreestanding -c boot.S -o boot.o
+clang --target=arm-none-eabihf -march=armv6 -mcpu=arm1176jzf-s -fpic -ffreestanding -c boot.S -o boot.o
