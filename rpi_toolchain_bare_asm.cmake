f1a2095ab0a994e681763b66d3aa8548b0d0604c
theorlangur <theorlangur@gmail.com>
2024-02-17 20:00:37 +0100

    basically building release with a debug information

--- rpi_toolchain_bare_asm.cmake
+++ rpi_toolchain_bare_asm.cmake
@@ -18,5 +18,6 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(CMAKE_NO_SYSTEM_FROM_IMPORTED 1)
 
 #set(CMAKE_CXX_FLAGS "-mcpu=arm1176jzf-s -fpic -ffreestanding -fno-exceptions -DPI_BARE -D_LIBCPP_HAS_NO_THREADS -isystem /usr/include/c++/v1 -isystem /usr/include")
-set(CMAKE_CXX_FLAGS "-S -fno-asynchronous-unwind-tables -fno-dwarf2-cfi-asm -mcpu=arm1176jzf-s -fpic -ffreestanding -fno-exceptions -DPI_BARE -D_LIBCPP_HAS_NO_THREADS -isystem ${pi_sdk}/usr/include/c++/v1 -isystem ${pi_sdk}/usr/include -isystem ${pi_sdk}/usr/include/arm-linux-gnueabihf")
+#-S -fno-asynchronous-unwind-tables -fno-dwarf2-cfi-asm -fverbose-asm
+set(CMAKE_CXX_FLAGS "-g -mcpu=arm1176jzf-s -fpic -ffreestanding -fno-exceptions -DPI_BARE -D_LIBCPP_HAS_NO_THREADS -isystem ${pi_sdk}/usr/include/c++/v1 -isystem ${pi_sdk}/usr/include -isystem ${pi_sdk}/usr/include/arm-linux-gnueabihf")
 set(CMAKE_EXE_LINKER_FLAGS "-L${bare_toolchain_path}/compiler-rt -l${builtins} -L${bare_toolchain_path} -T bare_link.ld -ffreestanding -nostdlib -fuse-ld=lld ${bare_toolchain_path}/boot.o")
