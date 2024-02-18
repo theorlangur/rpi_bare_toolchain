a49f7c80bb52ce6b06e0862b71b9f99d8125edab
theorlangur <theorlangur@gmail.com>
2024-02-18 21:24:43 +0100

    using clang 17

--- rpi_toolchain_bare.cmake
+++ rpi_toolchain_bare.cmake
@@ -1,5 +1,5 @@
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++-17)
+set(CMAKE_C_COMPILER clang-17)
 #eabi
 #set(triple arm-none-eabi)
 #set(builtins clang_rt.builtins-arm)
--- rpi_toolchain_bare_fake_pc.cmake
+++ rpi_toolchain_bare_fake_pc.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_CXX_COMPILER clang++)
-set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++-17)
+set(CMAKE_C_COMPILER clang-17)
 
 set(CMAKE_CXX_FLAGS "-fno-exceptions -DPI_BARE -DPI_BARE_FAKE -stdlib=libc++")
